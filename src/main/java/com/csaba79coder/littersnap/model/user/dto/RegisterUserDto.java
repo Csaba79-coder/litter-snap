b60735ea30ecf75711cb79203eb4e88f8a0e91f4
Csaba79-coder <csabavadasz79@gmail.com>
2023-05-22 18:44:27 +0200

    create developer branch for common collaboration

---
+++ src/main/java/com/csaba79coder/littersnap/model/user/dto/RegisterUserDto.java
@@ -0,0 +1,4 @@
+package com.csaba79coder.littersnap.model.user.dto;
+
+public class RegisterUserDto {
+}
--- src/main/java/com/csaba79coder/littersnap/model/user/persistence/UserRepository.java
+++ src/main/java/com/csaba79coder/littersnap/model/user/persistence/UserRepository.java
@@ -1,6 +1,6 @@
-package com.csaba79coder.littersnap.model.persistence;
+package com.csaba79coder.littersnap.model.user.persistence;
 
-import com.csaba79coder.littersnap.model.entity.user.User;
+import com.csaba79coder.littersnap.model.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
